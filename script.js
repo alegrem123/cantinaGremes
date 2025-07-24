ce7ac6692e44391c12c81ebee2070c1274de503a
alegrem123 <alessandro.gremes@studenti.unitn.it>
2025-07-24 14:59:50 +0200

    pre font change

--- script.js
+++ script.js
@@ -32,3 +32,19 @@
     });
   });
 
+const viniSwiper = new Swiper('.vini-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
